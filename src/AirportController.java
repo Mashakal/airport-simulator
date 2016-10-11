51ad0744b8db2b3199d009801686b4b1e8e2542a
Alex Neuenkirk <Alex Neuenkirk>
2016-10-11 18:14:48 -0400

    Added runways instantiation.

--- src/AirportController.java
+++ src/AirportController.java
@@ -2,8 +2,12 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class AirportController {
-	
+
+	private static final int NUM_RUNWAYS = 2;
+	private static final int TIME_SLOTS_TO_FULFILL = 3;
+
 	private static HashMap<Request.Type, ControllerData> data = new HashMap<>();
+	private static AirportRunway[] runways = new AirportRunway[NUM_RUNWAYS];
 	
 	private static final String planeStr = "Plane #%d has entered the %s queue.";
 	private static final String timeStr = "The time is: %d";
@@ -15,11 +19,17 @@ public class AirportController {
 			int first = (type == Request.Type.Landing) ? 1 : 2;
 			data.put(type, new ControllerData(first));
 		}
+
+		for (int i = 0; i < NUM_RUNWAYS; i++) {
+			runways[i] = new AirportRunway();
+		}
 	}
 
 
 	public void simulateTimeSlot(int iterations, int timeSlot) {
 		System.out.println(String.format(timeStr, timeSlot));
+
+		// Handle incoming requests.
 		for (int i = 0; i < iterations; i++) {
 			for (Request.Type type : Request.Type.values()) {
 				if (isTrue()) {
@@ -29,6 +39,8 @@ public class AirportController {
 				}
 			}
 		}
+
+		// Work the next requests.
 	}
 	
 	
